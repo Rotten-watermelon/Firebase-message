6a40fc676802262adc504c8196e405c5c961b711
yim sarakoudom <yim.oudom@gmail.com>
2020-07-10 14:06:30 +0700

    asdad

--- script1.js
+++ script1.js
@@ -182,3 +182,19 @@
   }
 
   resetUI();
+
+  window.addEventListener('load', e => {
+    // new PWAConfApp();
+     registerSW(); 
+   });
+   
+ 
+ async function registerSW() { 
+    // if ('serviceWorker' in navigator) { 
+       try {
+         await navigator.serviceWorker.register('firebase-messaging-sw.js'); 
+       } catch (e) {
+         alert('ServiceWorker registration failed.'); 
+       }
+    
+   }
